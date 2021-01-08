2d32713a5483087bc8ef8fd0302ab2b97dce2b90
mathieu.brunot <mathieu.brunot@monogramm.io>
2021-01-08 15:46:20 +0100

    :whale: Push images for Dodock
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

---
+++ images/v1.4/alpine3.12/bin/install_app.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+APP_NAME=${1}
+APP_REPO=${2}
+APP_BRANCH=${3}
+
+cd /home/dodock/dodock-bench/
+
+. env/bin/activate
+
+cd ./apps
+[ "${APP_BRANCH}" ] && BRANCH="-b ${APP_BRANCH}"
+git clone --depth 1 -o upstream ${BRANCH} ${APP_REPO} ${APP_NAME}
+sed -i -e 's/GitPython==2\.1\.11/GitPython==2\.1\.15/' ${APP_NAME}/requirements.txt
+grep 'ldap3' ${APP_NAME}/requirements.txt || echo -e '\nldap3==2.7' >> ${APP_NAME}/requirements.txt;
+pip3 install --no-cache-dir -e /home/dodock/dodock-bench/apps/${APP_NAME}
