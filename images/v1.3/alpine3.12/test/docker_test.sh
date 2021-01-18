3fb62625bc9440a62370852ce35c3b1d369ec79d
mathieu.brunot <mathieu.brunot@monogramm.io>
2021-01-18 23:52:30 +0100

    ✅ Improve Dodock tests
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- images/v1.3/alpine3.12/test/docker_test.sh
+++ images/v1.3/alpine3.12/test/docker_test.sh
@@ -37,6 +37,21 @@ if ! ping -c 10 -q dodock_web ; then
     exit 8
 fi
 
+echo "Checking bench environment..."
+bench doctor
+
+echo "Retrieving apps installed..."
+bench list-apps
+BENCH_LIST_APPS=$(bench list-apps)
+
+echo "Checking apps installed..."
+for app in frappe; do
+    if ! echo "$BENCH_LIST_APPS" | grep "$app"; then
+        echo "$app is not installed!"
+        exit 16
+    fi
+done
+
 
 ################################################################################
 # Success
@@ -49,56 +64,64 @@ echo 'Docker tests successful'
 # https://frappe.io/docs/user/en/testing
 ################################################################################
 
-FRAPPE_APP_TO_TEST=
+#DODOCK_APP_TO_TEST=frappe
 
-echo "Preparing Frappe application '${FRAPPE_APP_TO_TEST}' tests..."
+echo "Preparing Dodock application '${DODOCK_APP_TO_TEST}' tests..."
 
 bench set-config allow_tests true -g
 
-echo "Checking environment for '${FRAPPE_APP_TO_TEST}' tests..."
+echo "Checking environment for '${DODOCK_APP_TO_TEST}' tests..."
 bench doctor
 bench enable-scheduler
 bench doctor
 
 ################################################################################
-# TODO Frappe Unit tests
+# TODO Dodock Unit tests
 # https://frappe.io/docs/user/en/guides/automated-testing/unit-testing
 
-FRAPPE_APP_UNIT_TEST_REPORT="$(pwd)/sites/.${FRAPPE_APP_TO_TEST}_unit_tests.xml"
-FRAPPE_APP_UNIT_TEST_PROFILE="$(pwd)/sites/.${FRAPPE_APP_TO_TEST}_unit_tests.prof"
+DODOCK_APP_UNIT_TEST_REPORT="$(pwd)/sites/.${DODOCK_APP_TO_TEST}_unit_tests.xml"
+DODOCK_APP_UNIT_TEST_PROFILE="$(pwd)/sites/.${DODOCK_APP_TO_TEST}_unit_tests.prof"
 
 #bench run-tests --help
 
-if [ -n "${FRAPPE_APP_TO_TEST}" ]; then
+if [ -n "${DODOCK_APP_TO_TEST}" ]; then
 
-    echo "Executing Unit Tests of '${FRAPPE_APP_TO_TEST}' app..."
+    echo "Preparing dodock application '${DODOCK_APP_TO_TEST}' tests..."
+
+    bench set-config allow_tests true -g
+
+    bench doctor
+    bench enable-scheduler
+    bench doctor
+
+    echo "Executing Unit Tests of '${DODOCK_APP_TO_TEST}' app..."
     if [ "${TEST_VERSION}" = "10" ]; then
         bench run-tests \
-            --app "${FRAPPE_APP_TO_TEST}" \
-            --junit-xml-output "${FRAPPE_APP_UNIT_TEST_REPORT}" \
-            --profile > "${FRAPPE_APP_UNIT_TEST_PROFILE}"
+            --app "${DODOCK_APP_TO_TEST}" \
+            --junit-xml-output "${DODOCK_APP_UNIT_TEST_REPORT}" \
+            --profile > "${DODOCK_APP_UNIT_TEST_PROFILE}"
     else
         bench run-tests \
-            --app "${FRAPPE_APP_TO_TEST}" \
+            --app "${DODOCK_APP_TO_TEST}" \
             --coverage \
-            --junit-xml-output "${FRAPPE_APP_UNIT_TEST_REPORT}" \
-            --profile > "${FRAPPE_APP_UNIT_TEST_PROFILE}"
+            --junit-xml-output "${DODOCK_APP_UNIT_TEST_REPORT}" \
+            --profile > "${DODOCK_APP_UNIT_TEST_PROFILE}"
     fi
 
 fi
 
 ## Check result of tests
-if [ -f "${FRAPPE_APP_UNIT_TEST_REPORT}" ]; then
-    echo "Checking Frappe application '${FRAPPE_APP_TO_TEST}' unit tests report..."
+if [ -f "${DODOCK_APP_UNIT_TEST_REPORT}" ]; then
+    echo "Checking Dodock application '${DODOCK_APP_TO_TEST}' unit tests report..."
 
-    if grep -E '(errors|failures)="[1-9][0-9]*"' "${FRAPPE_APP_UNIT_TEST_REPORT}"; then
-        echo "Unit Tests of '${FRAPPE_APP_TO_TEST}' app failed! See logs for details."
-        #cat "${FRAPPE_APP_UNIT_TEST_REPORT}"
+    if grep -E '(errors|failures)="[1-9][0-9]*"' "${DODOCK_APP_UNIT_TEST_REPORT}"; then
+        echo "Unit Tests of '${DODOCK_APP_TO_TEST}' app failed! See logs for details."
+        #cat "${DODOCK_APP_UNIT_TEST_REPORT}"
         # FIXME Fix the tests in error
         #exit 1
     else
-        echo "Unit Tests of '${FRAPPE_APP_TO_TEST}' app successful!"
-        #cat "${FRAPPE_APP_UNIT_TEST_REPORT}"
+        echo "Unit Tests of '${DODOCK_APP_TO_TEST}' app successful!"
+        #cat "${DODOCK_APP_UNIT_TEST_REPORT}"
     fi
 fi
 
@@ -106,26 +129,26 @@ if [ -f ./sites/.coverage ]; then
     set +e
     cp ./sites/.coverage ./.coverage
 
-    echo "Unit Tests coverage report of '${FRAPPE_APP_TO_TEST}' app:"
+    echo "Unit Tests coverage report of '${DODOCK_APP_TO_TEST}' app:"
     coverage report -m
 
-    #echo "Sending Unit Tests coverage of '${FRAPPE_APP_TO_TEST}' app to Coveralls..."
-    #coveralls -b "$(pwd)/apps/${FRAPPE_APP_TO_TEST}" -d "$(pwd)/sites/.coverage"
+    #echo "Sending Unit Tests coverage of '${DODOCK_APP_TO_TEST}' app to Coveralls..."
+    #coveralls -b "$(pwd)/apps/${DODOCK_APP_TO_TEST}" -d "$(pwd)/sites/.coverage"
 
     # TODO When frappe supports coverage report in XML format
     # https://github.com/frappe/frappe/issues/9696
-    #echo "Sending Unit Tests coverage of '${FRAPPE_APP_TO_TEST}' app to Codacy..."
+    #echo "Sending Unit Tests coverage of '${DODOCK_APP_TO_TEST}' app to Codacy..."
     #wget -qO - https://coverage.codacy.com/get.sh | sh -s report -l Python -r "$(pwd)/sites/coverage.xml"
 
     rm ./.coverage
     set -e
 fi
 
-if [ -f "${FRAPPE_APP_UNIT_TEST_PROFILE}" ]; then
-    echo "Checking Frappe application '${FRAPPE_APP_TO_TEST}' unit tests profile..."
+if [ -f "${DODOCK_APP_UNIT_TEST_PROFILE}" ]; then
+    echo "Checking Dodock application '${DODOCK_APP_TO_TEST}' unit tests profile..."
 
     # XXX Are there any online services that could receive and display profiles?
-    #cat "${FRAPPE_APP_UNIT_TEST_PROFILE}"
+    #cat "${DODOCK_APP_UNIT_TEST_PROFILE}"
 fi
 
 
@@ -135,11 +158,11 @@ fi
 
 bench run-ui-tests --help
 
-#echo "Executing UI Tests of '${FRAPPE_APP_TO_TEST}' app..."
+#echo "Executing UI Tests of '${DODOCK_APP_TO_TEST}' app..."
 #if [ "${TEST_VERSION}" = "10" ] || [ "${TEST_VERSION}" = "11" ]; then
-#    bench run-ui-tests --app ${FRAPPE_APP_TO_TEST}
+#    bench run-ui-tests --app ${DODOCK_APP_TO_TEST}
 #else
-#    bench run-ui-tests ${FRAPPE_APP_TO_TEST}
+#    bench run-ui-tests ${DODOCK_APP_TO_TEST}
 #fi
 
 ## TODO Check result of UI tests
@@ -151,16 +174,16 @@ bench run-ui-tests --help
 
 #bench build-docs --help
 
-#echo "Generating docs for '${FRAPPE_APP_TO_TEST}' app..."
+#echo "Generating docs for '${DODOCK_APP_TO_TEST}' app..."
 #if [ "${TEST_VERSION}" = "10" ] || [ "${TEST_VERSION}" = "11" ]; then
 #    set +e
 #    bench build-docs \
-#        --target ${FRAPPE_APP_TO_TEST} \
-#        --docs-version ${FRAPPE_APP_TO_TEST} \
-#        ${FRAPPE_APP_TO_TEST}
+#        --target ${DODOCK_APP_TO_TEST} \
+#        --docs-version ${DODOCK_APP_TO_TEST} \
+#        ${DODOCK_APP_TO_TEST}
 #    set -e
 #else
-#    echo "Building docs is not available for this version of Frappe (${TEST_VERSION})"
+#    echo "Building docs is not available for this version of Dodock (${TEST_VERSION})"
 #fi
 
 ## TODO Check docs generated properly
@@ -168,6 +191,6 @@ bench run-ui-tests --help
 
 ################################################################################
 # Success
-echo "Frappe app '${FRAPPE_APP_TO_TEST}' tests finished"
+echo "Dodock app '${DODOCK_APP_TO_TEST}' tests finished"
 echo 'Check the CI reports and logs for details.'
 exit 0
