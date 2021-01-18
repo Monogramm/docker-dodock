824e0faf8080dc063deb958af81a7c1a70edfa5b
mathieu.brunot <mathieu.brunot@monogramm.io>
2021-01-18 13:29:24 +0100

    :sparkles: Improve Install app script
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- images/v1.3/slim-buster/bin/install_app.sh
+++ images/v1.3/slim-buster/bin/install_app.sh
@@ -4,14 +4,14 @@ APP_NAME=${1}
 APP_REPO=${2}
 APP_BRANCH=${3}
 
-cd /home/dodock/dodock-bench/
+cd /home/dodock/dodock-bench/ || exit
 
 . env/bin/activate
 
 echo "${APP_NAME}" > sites/apps.txt
-cd ./apps
+cd ./apps || exit
 [ "${APP_BRANCH}" ] && BRANCH="-b ${APP_BRANCH}"
-git clone --depth 1 -o upstream ${BRANCH} ${APP_REPO} ${APP_NAME}
-sed -i -e 's/GitPython==2\.1\.11/GitPython==2\.1\.15/' ${APP_NAME}/requirements.txt
-grep 'ldap3' ${APP_NAME}/requirements.txt || echo -e '\nldap3==2.7' >> ${APP_NAME}/requirements.txt;
-pip3 install --no-cache-dir -e /home/dodock/dodock-bench/apps/${APP_NAME}
+git clone --depth 1 -o upstream "${BRANCH}" "${APP_REPO}" "${APP_NAME}"
+sed -i -e 's/GitPython==2\.1\.11/GitPython==2\.1\.15/' "${APP_NAME}/requirements.txt"
+grep 'ldap3' "${APP_NAME}/requirements.txt" || echo -e '\nldap3==2.7' >> "${APP_NAME}/requirements.txt";
+pip3 install --no-cache-dir -e "/home/dodock/dodock-bench/apps/${APP_NAME}"
