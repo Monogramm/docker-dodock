ac5419b7f07df71f9d7e4d798aaa9036de669df0
mathieu.brunot <mathieu.brunot@monogramm.io>
2021-01-18 14:27:53 +0100

    :bug: Fix git clone
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- images/v1.4/alpine3.12/bin/install_app.sh
+++ images/v1.4/alpine3.12/bin/install_app.sh
@@ -11,7 +11,7 @@ cd /home/dodock/dodock-bench/ || exit
 echo "${APP_NAME}" > sites/apps.txt
 cd ./apps || exit
 [ "${APP_BRANCH}" ] && BRANCH="-b ${APP_BRANCH}"
-git clone --depth 1 -o upstream "${BRANCH}" "${APP_REPO}" "${APP_NAME}"
+git clone --depth 1 -o upstream ${BRANCH} "${APP_REPO}" "${APP_NAME}"
 sed -i -e 's/GitPython==2\.1\.11/GitPython==2\.1\.15/' "${APP_NAME}/requirements.txt"
 grep 'ldap3' "${APP_NAME}/requirements.txt" || echo -e '\nldap3==2.7' >> "${APP_NAME}/requirements.txt";
 pip3 install --no-cache-dir -e "/home/dodock/dodock-bench/apps/${APP_NAME}"
