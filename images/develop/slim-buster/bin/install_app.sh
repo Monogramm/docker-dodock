b5587a644dc470a85bd67b70ccae4d4077258d97
mathieu.brunot <mathieu.brunot@monogramm.io>
2021-04-11 04:46:28 +0200

    🚧 Fix bench urllib3 version to same as frappe
    Signed-off-by: mathieu.brunot <mathieu.brunot@monogramm.io>

--- images/develop/slim-buster/bin/install_app.sh
+++ images/develop/slim-buster/bin/install_app.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xe
 
 APP_NAME=${1}
 APP_REPO=${2}
@@ -13,10 +13,10 @@ cd ./apps || exit
 [ "${APP_BRANCH}" ] && BRANCH="-b ${APP_BRANCH}"
 git clone --depth 1 -o upstream ${BRANCH} "${APP_REPO}" "${APP_NAME}"
 sed -i -e 's/GitPython==2\.1\.11/GitPython==2\.1\.15/' "${APP_NAME}/requirements.txt"
-#sed -i -e 's/urllib3==.*/urllib3/' "${APP_NAME}/requirements.txt"
+#sed -i -e 's/urllib3==.*/urllib3==1.25.10/' "${APP_NAME}/requirements.txt"
 grep 'ldap3' "${APP_NAME}/requirements.txt" || echo -e '\nldap3==2.7' >> "${APP_NAME}/requirements.txt";
 pip3 install --no-cache-dir -e "/home/dodock/dodock-bench/apps/${APP_NAME}"
-cd ${APP_NAME}
+cd "${APP_NAME}"
 yarn
 yarn run production
 yarn install --production=true
